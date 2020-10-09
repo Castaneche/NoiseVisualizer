1a937960760745071bfa6ad1f5dea98c24decefa
Castaneche <babou420@free.fr>
2020-10-09 15:40:21 +0200

    opengl installation successful

--- Source.cpp
+++ Source.cpp
@@ -1,99 +1,124 @@
-#include <imgui.h>
-#include <imgui-SFML.h>
-#include <theme.h>
-#include <wrappers.h>
-#include <implot/implot.h>
-
-#include <SFML/Graphics.hpp>
-
 #include <iostream>
 
-#include <PerlinNoise1DVisualizer.h>
-#include <PerlinNoise2DVisualizer.h>
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "implot.h"
+
+#include <theme.h>
+
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
+	// Setup window
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	sf::RenderWindow window(sf::VideoMode(700, 600), "Noise Visualizer", sf::Style::Default);
-	window.setVerticalSyncEnabled(true);
-	ImGui::SFML::Init(window);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	//Opengl Loader
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ApplyTheme6();
+
+	// Setup Platform/Renderer bindings
+	const char* glsl_version = "#version 130";
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	ImFont* font = io.Fonts->AddFontFromFileTTF("res/font/bebas/Bebas-Regular.ttf", 17.0f);
+	IM_ASSERT(font != NULL);
+
+	// Setup ImPlot context
 	ImPlot::CreateContext();
 	ImPlot::GetStyle().AntiAliasedLines = false;
 
-	ApplyTheme6();
-	ImGuiIO& io = ImGui::GetIO();
-	ImFont* font = io.Fonts->AddFontFromFileTTF("res/font/bebas/Bebas-Regular.ttf", 17.0f);
-	ImGui::SFML::UpdateFontTexture();
 
-	ImGuiStyle& style = ImGui::GetStyle();
-	style.ScaleAllSizes(1);
+	int display_w = 0, display_h = 0;
+	while (!glfwWindowShouldClose(window))
+	{
+		//inputs
+		glfwPollEvents();
 
-	if (font == nullptr) std::cout << "Error";
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
-	window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
-	sf::Clock deltaClock;
-	sf::Clock fpsClock;
-	sf::Time delta;
-	int fpsLogRate = 200;
-	float fpsvalues[50] = { 0 };
-
-	PerlinNoise1DVisualizer PN1DVisualizer(500);
-	PerlinNoise2DVisualizer PN2DVisualizer(300, 150);
-
-	while (window.isOpen()) {
-		sf::Event event;
-
-		while (window.pollEvent(event)) {
-			ImGui::SFML::ProcessEvent(event);
-
-			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-				window.close();
-		}
-
-		delta = deltaClock.restart();
-
-		// ===== FPS CALCULATION ====
-		float fps = 1.0f / delta.asSeconds();
-
-		if (fpsClock.getElapsedTime().asMilliseconds() >= fpsLogRate) {
-			for (int i = 0; i < 49; i++)
-			{
-				fpsvalues[i] = fpsvalues[i + 1];
-			}
-			fpsvalues[49] = fps;
-			fpsClock.restart();
-		}
-
-
-		ImGui::SFML::Update(window, delta);
-		ImGui::PushFont(font);
 
 		ImGui::Begin("Perlin Noise", 0, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
 		ImGui::SetWindowPos(ImVec2(0, 0));
-		ImGui::SetWindowSize(ImVec2(float(window.getSize().x), float(window.getSize().y)));
-
+		ImGui::SetWindowSize(ImVec2(display_w, display_h));
 		if (ImGui::CollapsingHeader("[1D] Perlin Noise : Graph, Hills"))
 		{
-			PN1DVisualizer.Show();
+			// ...
 		}
 		if (ImGui::CollapsingHeader("[2D] Perlin Noise : Texture, Terrain"))
 		{
-			PN2DVisualizer.Show();
+			// ...
 		}
 
 		ImGui::SetCursorPosX(280);
 		ImGui::Text("Click on headers to open them");
 		ImGui::End();
+		
 
-		ImGui::PopFont();
+		// Rendering
+		ImGui::Render();
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(1, 1, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		window.clear(sf::Color::Black); // fill background with color
-		ImGui::SFML::Render(window);
-		window.display();
+		glfwSwapBuffers(window);
 	}
 
+	// Cleanup
 	ImPlot::DestroyContext();
-	ImGui::SFML::Shutdown();
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
